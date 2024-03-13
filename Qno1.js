786020aaafa9341d4e2264f0c7c2320e5a6121ae
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-13 23:55:12 +0500

    Add files via upload

---
+++ Qno1.js
@@ -0,0 +1,9 @@
+"use strict";
+// 1. Install Node.js, TypeScript and VS Code on your computer.
+Object.defineProperty(exports, "__esModule", { value: true });
+// ANSWER FIRST YOU DAWNLODE NODE JS AND VS CODE THAN  CLICK NODE COMMAND PROMT AND OPEN 
+// 1ST STEP : node -v CHK NODE VERSION
+// 2ND STEP : npm install typescript global 3.2.5
+// 3RD STEP : CHK TS (TYPESCRIPT) VERSION YOU WRITE A tsc -v
+// 4TH STEP : WRITE A code . 
+//         YOU GO TO VS(VISUAL STUDIO CODE) AND WRITE A CODE 
--- Qno1.ts
+++ Qno1.ts
@@ -1,7 +1,12 @@
+// 1. Install Node.js, TypeScript and VS Code on your computer.
 
-// Q1.Install Node.js, TypeScript and VS Code on your computer.
+// ANSWER FIRST YOU DAWNLODE NODE JS AND VS CODE THAN  CLICK NODE COMMAND PROMT AND OPEN 
+// 1ST STEP : node -v CHK NODE VERSION
+// 2ND STEP : npm install typescript global 3.2.5
+// 3RD STEP : CHK TS (TYPESCRIPT) VERSION YOU WRITE A tsc -v
+// 4TH STEP : WRITE A code . 
+//         YOU GO TO VS(VISUAL STUDIO CODE) AND WRITE A CODE 
 
-1.// https://code.visualstudio.com/download for (vs code)
-2. //nodejs.org/en/download for (nodejs)
-3.// npm i -g typescript  for (typescript)
-4. //tsc -- init  for (tsconfig.json)
+
+
+
